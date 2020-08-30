29e61d28f06486115131a8033d6b2fa00e5a04f5
Gerardo Gonzales <gerargd@outlook.es>
2020-08-30 06:38:48 -0600

    Working on epsilonnfa

--- Automatas.py
+++ Automatas.py
@@ -1,17 +1,68 @@
 import json
-
+from collections import OrderedDict
 class Automatas:
 
   def definirTipo(self,transiciones):
     return 1
 
         
-  def nfa_dfa(self,alphabet, states, initial_state, accepting_states, transitions):
-    alfabetoNFA=alphabet
-    statesNFA=states
-    initial_stateNFA=initial_state
-    accepting_statesNFA=accepting_states
-    transitionsNFA=transitions
+  def nfae_dfa(self,alphabet, states, initial_state, accepting_states, transitions):
+   
+   cerraduraEpsilon=[]
+
+   for est in states:
+     newCerr = ''
+     for trans in transitions:
+       if trans[0]==est and trans[1]=='e':
+         newCerr=newCerr+trans[2]
+     newCerr=newCerr+est
+     cerraduraEpsilon.append((est,newCerr))
+    
+   
+   for i in range(len(cerraduraEpsilon)):
+      newword = list(OrderedDict.fromkeys(cerraduraEpsilon[i][1]))
+      concatword=""
+      for c in newword:
+          concatword= concatword+c
+      cerraduraEpsilon[i]=(cerraduraEpsilon[i][0],concatword)
+   deltaCerraduraEpsilon=[]
+
+   for alph in alphabet:
+     #print(alph)
+     for tuples in cerraduraEpsilon:
+       newDeltWord=""
+       for char in tuples[1]:
+         #print(char)
+         for trans in transitions:
+           if char==trans[0] and alph==trans[1]:
+             #print(trans)
+             newDeltWord=newDeltWord+trans[2]
+       #print(newDeltWord) 
+       #print(trans[1])
+       deltaCerraduraEpsilon.append((newDeltWord,alph,tuples[0]))
+
+   cerraduradeltaEpsilon=[]
+   
+   for delt in deltaCerraduraEpsilon:
+     #print(delt)
+     for char in delt[0]:
+       #print(char)
+       for trand in cerraduraEpsilon:
+          if char==
+
+
+
+
+        
+      
+      
+
+   
+   
+
+ 
+
+  
 
 
 
--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 import json
 from Evaluadores import *
+from Automatas import *
 
+alfabeto = []
+estados = []
+estados_iniciales = []
+estados_aceptados = []
+transiciones = {}
 with open('evaluando.json') as contenido:
  defincionAutomata = json.load(contenido)
  for automataValores in defincionAutomata['Automata']:
@@ -9,7 +15,9 @@ with open('evaluando.json') as contenido:
         estados_iniciales = automataValores['initial_state']
         estados_aceptados = automataValores['accepting_states']
         transiciones = automataValores['transitions']
-
-
+                  
+ev=Automatas()
+ev.nfae_dfa(alfabeto,estados,estados_iniciales,estados_aceptados,transiciones)
+#nfa_transition_dict[(starting_state, transition_symbol)] = [ending_state]
 
 
